582799ad6757406f992e7ad280d415ae8949f32d
KaiserPhemi <kaiser.phemi@protonmail.ch>
2022-08-01 00:00:52 +0100

    chore()

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,24 @@
 // Nest Libraries
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+
+// services
+import { AuthService } from './auth.service';
 
 /**
  * Controller for authenticating users.
  */
 @Controller('auth')
-export class AuthController { }
+export class AuthController {
+  constructor(private authService: AuthService) { }
+
+  @Post('signup')
+  signup() {
+    return "I did sign up"
+  }
+
+  @Post('login')
+  login() {
+    return "I did log in"
+  }
+}
+
